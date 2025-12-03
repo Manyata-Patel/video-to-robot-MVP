ce1fcef22cc1889cdcd0f2b362a5eadbb0103a4b
Manyata-Patel <138508703+Manyata-Patel@users.noreply.github.com>
2025-12-03 08:39:30 -0600

    MP - Requirements code

---
+++ capture_midas.py
@@ -0,0 +1,11 @@
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import torch
+import torchvision.transforms as T
+import time
+import json
+import os
+from pathlib import Path
+from tqdm import tqdm
